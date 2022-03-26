831fe77f54cb8cf603c50d264293e5300dc829ed
limonzishad <zishadlimon@gmail.com>
2022-03-26 21:57:46 +0600

    2nd question corrected

--- src/components/QnA/QnA.js
+++ src/components/QnA/QnA.js
@@ -15,12 +15,12 @@ const QnA = () => {
         <p className="text">React implements a virtual DOM which is a DOM tree representation in JavaScript. This virtual DOM find the most efficient way to update the browser's DOM. React elements are plain objects and cheap to create. Besides, JavaScript is quite fast. So, virtual DOM of React updates the browser's DOM to match the React elements.</p>
       </div>
       <div className="qna-container">
-        <p style={{ fontWeight: 700, marginTop: 0, fontSize: 20 }}>Q: Difference between props and use state?</p>
-        <p className="text">Props is short form for properties. It is passed from a parent component to a child component. State determine the current condition of the component in a React application. The React useState Hook tracks the state in a function component.</p>
+        <p style={{ fontWeight: 700, marginTop: 0, fontSize: 20 }}>Q: Difference between props and state?</p>
+        <p className="text">Props is short form for properties. It is passed from a parent component to a child component. Props are read only and it is immutable. State determine the current condition of the component in a React application. State cannot be accessed by child components. State is mutable.</p>
       </div>
       <div id="gap" className="qna-container">
         <p style={{ fontWeight: 700, marginTop: 0, fontSize: 20 }}>Q: How useState works?</p>
-        <p className="text">useState is a Hook that allows to have state variables in functional components. The the initial state is passed to this function. Likewise, it returns a variable with the current state value. Furthermore, it returns another function to update the value.</p>
+        <p className="text">useState is a Hook that allows to have state variables in functional components. It tracks the state in a function component. The the initial state is passed to this function. Likewise, it returns a variable with the current state value. Furthermore, it returns another function to update the value.</p>
       </div>
     </div>
   );
